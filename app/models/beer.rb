30a9b7bf4825221dab753f1dbabdec764095def1
areunamo <antti.reunamo@helsinki.fi>
2014-01-25 18:16:23 +0200

    2. teht. 1-8 välitallennus

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,30 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
+  has_many :ratings
+
+  def average_rating
+	if ratings.empty?
+ 		"no ratings"
+	else
+		# inject
+		#ratings.inject(0.0) { |sum, rating| sum + rating.score } / ratings.count
+
+		# ActiveRecord
+		#ratings.average("score")
+
+		# vanha tapa
+		#@score = 0.0	# desimaalit mukaan!
+ 		#ratings.each do |rating|
+   		#	@score = @score + rating.score
+   		#end
+   		#@score = @score / ratings.count
+ 		##{@score}"
+	end
+  end
+
+  def to_s
+  		"#{name} (#{brewery.name})"
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Ratebeer::Application.routes.draw do
+
+  root 'breweries#index'
+
+  get 'kaikki_bisset', to: 'beers#index'
+
+  get 'ratings', to: 'ratings#index'
+
+  get 'ratings/new', to: 'ratings#new'
+
+  post 'ratings', to: 'ratings#create'
+
   resources :beers
 
   resources :breweries
