677fa318d572f3b2475d4ead28799b54b774ea47
areunamo <antti.reunamo@helsinki.fi>
2014-01-27 00:21:39 +0200

    2. tehtävät

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,26 +1,13 @@
 class Beer < ActiveRecord::Base
+
+  include RatingAverage
+
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
 
-  def average_rating
-	if ratings.empty?
- 		"no ratings"
-	else
-		# inject
-		#ratings.inject(0.0) { |sum, rating| sum + rating.score } / ratings.count
 
-		# ActiveRecord
-		#ratings.average("score")
 
-		# vanha tapa
-		#@score = 0.0	# desimaalit mukaan!
- 		#ratings.each do |rating|
-   		#	@score = @score + rating.score
-   		#end
-   		#@score = @score / ratings.count
- 		##{@score}"
-	end
-  end
+
 
   def to_s
   		"#{name} (#{brewery.name})"
---
+++ lib/rating_average.rb
@@ -0,0 +1,22 @@
+module RatingAverage
+
+  def average_rating
+	if ratings.empty?
+ 		"no ratings"
+	else
+		# inject
+		#ratings.inject(0.0) { |sum, rating| sum + rating.score } / ratings.count
+
+		# ActiveRecord
+		ratings.average("score")
+
+		# vanha tapa
+		#@score = 0.0	# desimaalit mukaan!
+ 		#ratings.each do |rating|
+   		#	@score = @score + rating.score
+   		#end
+   		#@score = @score / ratings.count
+ 		##{@score}"
+	end
+  end
+end
