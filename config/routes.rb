c20a631d5ab120f42306369591c122f662098b96
areunamo <antti.reunamo@helsinki.fi>
2014-01-26 23:45:33 +0200

    2. tehtävät

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Ratebeer::Application.routes.draw do
 
   get 'kaikki_bisset', to: 'beers#index'
 
-  get 'ratings', to: 'ratings#index'
+  #get 'ratings', to: 'ratings#index'
+  #get 'ratings/new', to: 'ratings#new'
+  #post 'ratings', to: 'ratings#create'
 
-  get 'ratings/new', to: 'ratings#new'
-
-  post 'ratings', to: 'ratings#create'
+  resources :ratings, only: [:index, :new, :create, :destroy]
 
   resources :beers
 
